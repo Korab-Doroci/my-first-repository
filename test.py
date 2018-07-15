5ab126a1977d44acbe9e68f566c4dd01a4e9d3af
korab-doroci <korab.doroci@gmail.com>
2018-07-15 14:08:37 +0200

    i added loggin functionality

--- test.py
+++ test.py
@@ -13,9 +13,21 @@ class User:
     def show_name(self):
         return str("this users name is: " + self.name)
 
+    def loggin(self):
+        loggin_input_name = str(input("input your username here "))
+        loggin_input_password = str(input("input your password here "))
+
+        if loggin_input_name == self.name and \
+                loggin_input_password == self.password:
+            print("your logged in as " + self.name)
+        else:
+            print("the name or password is wrong try again ")
+
 
 user_input = input("what is your name? ")
 
-user_password_input = input("create a password for your account")
+user_password_input = input("create a password for your account ")
 
 user1 = User(str(user_input), str(user_password_input))
+
+user1.loggin()
